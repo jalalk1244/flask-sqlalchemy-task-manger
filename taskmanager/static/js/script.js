09fd8ca231c615fd71acdbac61e5fa048494b41d
jalalk1244 <jalaluddin.boss@gmail.com>
2022-11-03 12:21:32 +0000

    Add basic frontend content

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenav initilalization
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
