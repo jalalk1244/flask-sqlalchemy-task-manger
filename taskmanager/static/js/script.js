b30e6d841b24112effb181561f67467de244be90
jalalk1244 <jalaluddin.boss@gmail.com>
2022-11-04 14:47:01 +0000

    Add a modal

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // sidenav initilalization
+    // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+    //  Modal initialization
+    let  modal = document.querySelectorAll('.modal');
+    M.Modal.init(modal);
   });
